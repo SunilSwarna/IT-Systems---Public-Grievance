3bbac42beb5530abf03f566c49c49ef45003560e
shanmukh1210 <shanmukh.k14@iiits.in>
2017-02-15 11:11:51 +0530

    some shit

--- shanmukh/PublicGrievance/app/src/main/java/app/AppConfig.java
+++ shanmukh/PublicGrievance/app/src/main/java/app/AppConfig.java
@@ -5,10 +5,10 @@ package app;
  */
 public class AppConfig {
     // Server user login url
-    public static String URL_LOGIN = "http://192.168.0.102/android_login_api/login.php";
+    public static String URL_LOGIN = "http://192.168.2.45/android_login_api/login.php";
 
     // Server user register url
-    public static String URL_REGISTER = "http://192.168.0.102/android_login_api/register.php";
+    public static String URL_REGISTER = "http://192.168.2.45/android_login_api/register.php";
 }
 
 
